b9d27254da9f2e4594222f868e81055c48db89df
Csaba Králik <kralikcsabi@gmail.com>
2022-06-01 08:05:20 +0200

    landing page

---
+++ project/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="welcome-text">
+      <h1>Welcome to Notes!</h1>
+      <h2>
+        Dump your mind, brainstorm with groups or just write shopping lists
+        <br />
+        <br />
+        with Notes!
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
